77901d10cdabf1a7936c0f3fb1e3c2b0d097cfc3
Mazen-Muhamed <Mazen-Muhamed@outlook.com>
2026-07-26 00:49:27 +0300

    CRUD APU w SQL

--- main.py
+++ main.py
@@ -1,44 +1,78 @@
-from fastapi import FastAPI, HTTPException
-from typing import Optional
+import sqlite3
+
+
+from fastapi import FastAPI, HTTPException, Depends, Response
+from typing import Optional, List
 from pydantic import BaseModel
-from sqlmodel import SQLModel, Field, create_engine, Session, select
 
-app = FastAPI()
+app = FastAPI(title="Integration CRUT w SQL")
 
 
-class Task(SQLModel, table=True):
-    task_id : int = Field(default=None, primary_key=True)
-    task_name : str
-    task_description : Optional[str] = None
-    task_status : bool = False
+DB_PATH = "tasks.db"
 
-class CreateTask(SQLModel):
+def get_db():
+    conn = sqlite3.connect(DB_PATH, check_same_thread=False)
+    conn.row_factory = sqlite3.Row          # Rows be like dicts
+    try:
+        yield conn
+    finally:
+        conn.close()
+
+
+def init_db():
+    conn = sqlite3.connect(DB_PATH)
+
+    conn.execute("DROP TABLE IF EXISTS tasks")
+
+    conn.execute("""
+                CREATE TABLE IF NOT EXISTS tasks
+        (
+            task_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            task_name TEXT NOT NULL,
+            task_description TEXT,
+            task_status BOOLEAN NOT NULL DEFAULT 0
+            )
+            """)
+
+    count = conn.execute("SELECT COUNT(*) FROM tasks").fetchone()
+    if count[0] == 0:
+        try:
+            conn.execute("BEGIN")
+            conn.executemany(
+                "INSERT INTO tasks (task_name, task_description, task_status) VALUES (?, ?, ?)",
+                [
+                    ("Buy Groceries", "2% from the store", 0),
+                    ("Make Assignment", "Make coding Assignment", 1),
+                    ("Study AI", "Build AI Agentic Model", 0),
+                ],
+            )
+            conn.commit()
+        except Exception:
+            conn.rollback()
+            raise
+
+    conn.close()
+
+@app.on_event("startup")
+def startup():
+    init_db()
+
+class CreateTask(BaseModel):
     task_name : str
     task_description : Optional[str] = None
     task_status : Optional[bool] = False
 
-engine = create_engine(
-    "sqlite:///tasks.db",
-    connect_args={
-        "check_same_thread" : False})
 
-def getSession():
-    with Session(engine) as session:
-        yield session
+class TaskResponse(BaseModel):
+    task_id : int
+    task_name: str
+    task_description: Optional[str] = None
+    task_status: Optional[bool] = False
 
-@app.on_event("startup")
-def onStartup():
-    SQLModel.metadata.create_all(engine)
+    class Config:
+        from_attributes = True
 
-    with Session(engine) as session:
-        existing = session.exec(select(Task)).all()
-        if len(existing) == 0:
-            session.add_all([
-                Task(task_name="Buy Groceries", task_description="2% from the store", task_status=False),
-                Task(task_name="Make Assignment", task_description="Make coding Assignment", task_status=True),
-                Task(task_name="Study AI", task_description="Build AI Agentic Model", task_status=False),
-            ])
-            session.commit()
+                        #### EndPoints ###
 
 @app.get('/health')
 def health():
@@ -46,53 +80,62 @@ def health():
 
 @app.get('/')
 def root():
-    return {"name": "Task API", "version": "1.0", "endpoints": ["/tasks"]}
-
-@app.get('/tasks')
-def getAllTasks():
-    return tasks_db
+    return {"Message": "Intro to FastAPI w/ raw SQL"}
 
 
-@app.get('/tasks/{task_id}')
-def getTaskById(task_id: int):
-    for task in tasks_db:
-        if task["task_id"] == task_id:
-            return task
-    raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
+@app.get('/tasks', response_model=List[TaskResponse])
+def get_all_tasks(db: sqlite3.Connection = Depends(get_db)):
+    rows = db.execute("SELECT * FROM tasks").fetchall()
+    return [dict(row) for row in rows]
 
-@app.post('/tasks', status_code=201)
-def createTask(task : CreateTask):
+@app.get('/tasks/{task_id}', response_model=TaskResponse)
+def getTaskById(task_id: int, db:sqlite3.Connection = Depends(get_db)):
+    row = db.execute("SELECT * FROM tasks WHERE task_id = ?", (task_id,)).fetchone()
+    if not row:
+        raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
+    return dict(row)
+
+@app.post('/tasks', response_model=TaskResponse, status_code=201)
+def createTask(task: CreateTask, db:sqlite3.Connection = Depends(get_db)):
     if not task.task_name.strip():
         raise HTTPException(status_code=400, detail="Task name required")
-    new_id = max([t["task_id"] for t in tasks_db]) + 1 if tasks_db else 1
+    cursor = db.execute(
+        "INSERT INTO tasks (task_name, task_description, task_status) VALUES (?, ? ,?)",
+        (task.task_name, task.task_description, task.task_status)
+    )
+    db.commit()
+    new_id = cursor.lastrowid
 
-    new_task = {
-        "task_id" : new_id,
-        "task_name" : task.task_name,
-        "task_description" : task.task_description,
-        "task_status" : False,
-    }
+    row = db.execute("SELECT * FROM tasks WHERE task_id = ?", (new_id)).fetchone()
+    return dict(row)
 
-    tasks_db.append(new_task)
-    return new_task
+@app.put('/tasks/{task_id}', response_model=TaskResponse)
+def updateTask(task_id: int, task: CreateTask, db: sqlite3.Connection = Depends(get_db)):
+    existing = db.execute("SELECT * FROM tasks WHERE task_id = ?", (task_id)).fetchone()
+    if not existing:
+        raise HTTPException(status_code=404, detail="Task does not exist!")
+    if not task.task_name.strip():
+        raise HTTPException(status_code=400, detail="Task Name Required!")
 
-@app.put('/tasks/{task_id}')
-def updateTask(task_id: int, updated: CreateTask):
-    for task in tasks_db:
-        if task["task_id"] == task_id:
-            if not updated.task_name.strip():
-                raise HTTPException(status_code=400, detail="Task name required")
-            task["task_name"] = updated.task_name
-            task["task_description"] = updated.task_description
-            task["task_status"] = updated.task_status
-            return task
-    raise HTTPException(status_code=404, detail=f"Task {task_id} Not Found! ")
+    db.execute("UPDATE tasks SET task_name = ?, task_description = ? , task_status = ? WHERE task_id = ?",
+               (task.task_name, task.task_description, task.task_status, task_id),
+               )
+    db.commit()
+
+    row = db.execute("SELECT * FROM tasks WHERE task_id = ?", (task_id)).fetchone()
+    return dict(row)
+
+
 
 @app.delete('/tasks/{task_id}', status_code=204)
-def deleteTask(task_id : int):
-    for task in tasks_db:
-        if task["task_id"] == task_id:
-            tasks_db.remove(task)
-            return {f"Task Removed {task_id}"}
-    raise HTTPException(status_code=404, detail=f"Task {task_id} Not Found! ")
+def deleteTask(task_id : int, db: sqlite3.Connection = Depends(get_db)):
+    existing = db.execute("SELECT * FROM tasks WHERE task_id = ? ", (task_id)).fetchone()
+
+    if not existing:
+        raise HTTPException(status_code=404, detail="Task does not exist! ")
+
+    db.execute("DELETE FROM tasks WHERE task_id = ?", (task_id,))
+    db.commit()
+    return Response(status_code=204)
+
 
